7ec75194bd9934d92c145ca507bc5dddc4d83c0c
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-06-01 02:06:57 +0530

    login functionality added

---
+++ nuxt-login/composables/api.ts
@@ -0,0 +1,26 @@
+
+import axios from "axios"
+// import { UserCRedentials } from "./types"
+
+// username: 'kminchelle',
+// password: '0lelplR',
+
+
+// export interface LoginUser {
+//     user:UserCRedentials
+// }
+
+export const getUser=async(email:string,password:string)=>{
+    try{
+        const data=await axios.post("https://dummyjson.com/auth/login",{
+            username:email,
+            password:password
+        })
+
+        const res=await data.request;
+
+        return res
+    }catch(err){
+        return false
+    }
+}
---
+++ nuxt-login/pages/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Login</h1>
+
+        <div>
+            <form @submit.prevent="handleLogin">
+                <label>Email</label>
+                <input type="text" class="form-input">
+                <label>
+                    Password
+                </label>
+                <input type="password" class="form-input">
+                <input type="submit" class="btn btn-secondary" >
+            </form>
+        </div>
+
+        <br/>
+        <div>
+            //         username: 'kminchelle',
+    // password: '0lelplR',
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import {getUser} from "~/composables/api"
+export default Vue.extend({
+    methods:{
+        async handleLogin(){
+    
+            const res=getUser("kminchelle","0lelplR");
+
+            console.log(res)
+        }
+    }
+})
+</script>
